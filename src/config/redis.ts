da682ea05db548cbb51e480e7a7a6d40cb7a8aaf
Joe Reeve <joseph.reeve@googlemail.com>
2019-05-19 14:45:38 +0200

    :fire: remove dead files

--- src/config/redis.ts
+++
@@ -1,8 +0,0 @@
-export const redisDefault =
-process.env.REDIS_URI ||
-process.env.REDISCLOUD_URL || // heroku
-(process.env.REDIS_PORT_6379_TCP_ADDR // docker
-  ? `redis://${process.env.REDIS_PORT_6379_TCP_ADDR}:${
-      process.env.REDIS_PORT_6379_TCP_PORT
-    }`
-  : 'redis://localhost:6379') // native
